616e96e0a39b6dd7b6ef5a458ca57bf4698bdf8c
Jana Ahurtsova <byjankaaa@gmail.com>
2022-07-04 10:50:13 +0300

    feat: add a solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function toReadable (number) {
   
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let numberString = number.toString();
+  
+    if (number === 0) {
+        return 'zero';
+    } 
+    if (number < 10) {
+        return ones[number];
+    }
+    if (numberString.length === 2 && numberString[0] == 1) {
+        return teens[numberString[1]];
+    }
+
+    if (numberString.length === 2) {
+        if (numberString[1] == 0) {
+            return tens[numberString[0]];
+        } else if (numberString[0] !== 1 && numberString[1] !== 0) {
+            return tens[numberString[0]] + ' ' + ones[numberString[1]];
+        } 
+    }
+
+    if (numberString.length === 3) {
+        if (numberString[1] == 0 && numberString[2] == 0) {
+            return ones[numberString[0]] + ' hundred';
+        } else if (numberString[1] == 0) {
+            return ones[numberString[0]] + ' hundred ' + ones[numberString[2]];
+        } else if (numberString[1] == 1) {
+            return ones[numberString[0]] + ' hundred ' + teens[numberString[2]];
+        } else if(numberString[1] !== 0 && numberString[1] !== 1 && numberString[2] == 0) {
+            return ones[numberString[0]] + ' hundred ' + tens[numberString[1]];
+        } else {
+            return ones[numberString[0]] + ' hundred ' + tens[numberString[1]] + ' ' + ones[numberString[2]];
+        }
+    }
 }
+ 
+
+
